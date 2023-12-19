0c2682fa203d526b772ea01009988ecf6fcf482f
izaanahmed55 <izaanahmedkhan@gmail.com>
2023-12-19 20:18:06 +0500

    Toggle Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,29 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
- 
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+// import { cn } from "@/lib/utils";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body 
+            // className={cn(font.className,"bg-white dark:bg-[#313338]")}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }

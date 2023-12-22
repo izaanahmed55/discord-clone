ed999c45b37dbac4a581d1be41d827f08faa182e
izaanahmed55 <izaanahmedkhan@gmail.com>
2023-12-22 19:27:37 +0500

    Server Creation

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -55,7 +55,7 @@ export const InitialModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/server", values)
+      await axios.post("/api/servers", values)
 
       form.reset()
       router.refresh()
